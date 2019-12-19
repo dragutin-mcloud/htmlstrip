9a000513b70920503bed1082574d7228f5994cea
Dragutin Cirkovic <dragutin@mcloud.rs>
2019-12-19 14:17:42 +0100

    Add travis build and fix tests

--- strip_test.go
+++ strip_test.go
@@ -2,7 +2,7 @@ package htmlstrip
 
 import "testing"
 
-func TestStrip(t *testing.T) {
+func TestStrip1(t *testing.T) {
 
 	result := StripHTML([]byte("<html>TEST</html>"))
 
@@ -11,123 +11,139 @@ func TestStrip(t *testing.T) {
 	} else {
 		t.Fail()
 	}
-
+}
+func TestStrip2(t *testing.T) {
+	result := StripHTML([]byte("<html>TEST</html>"))
 	if string(result) == "test" {
 		t.Log("Correct sentence returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("BEFORE<html>TEST</html>AFTER"))
+}
+func TestStrip3(t *testing.T) {
+	result := StripHTML([]byte("BEFORE<html>TEST</html>AFTER"))
 	if string(result) == "beforetestafter" {
 		t.Log("Extended sentence BEFORE<html>TEST</html>AFTER returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("BEFORE<html>TEST</h"))
+}
+func TestStrip4(t *testing.T) {
+	result := StripHTML([]byte("BEFORE<html>TEST</h"))
 	if string(result) == "beforetest" {
 		t.Log("Extended sentence BEFORE<html>TEST</h returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("BEFORE<ht"))
+}
+func TestStrip5(t *testing.T) {
+	result := StripHTML([]byte("BEFORE<ht"))
 	if string(result) == "before" {
 		t.Log("Extended sentence BEFORE<ht returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("BEFORE<html>"))
+}
+func TestStrip6(t *testing.T) {
+	result := StripHTML([]byte("BEFORE<html>"))
 	if string(result) == "before" {
 		t.Log("Extended sentence BEFORE<html> returned OK")
 	} else {
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("<html>AFTER"))
+}
+func TestStrip7(t *testing.T) {
+	result := StripHTML([]byte("<html>AFTER"))
 	if string(result) == "after" {
 		t.Log("Extended sentence <html>AFTER returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("html>AFTER"))
+}
+func TestStrip8(t *testing.T) {
+	result := StripHTML([]byte("html>AFTER"))
 	if string(result) == "html>after" {
 		t.Log("Extended sentence html>AFTER returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("<>"))
+}
+func TestStrip9(t *testing.T) {
+	result := StripHTML([]byte("<>"))
 	if string(result) == "" {
 		t.Log("Extended sentence <> returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte(">"))
+}
+func TestStrip10(t *testing.T) {
+	result := StripHTML([]byte(">"))
 	if string(result) == ">" {
 		t.Log("Extended sentence > returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("<"))
+}
+func TestStrip11(t *testing.T) {
+	result := StripHTML([]byte("<"))
 	if string(result) == "" {
 		t.Log("Extended sentence returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("<<"))
+}
+func TestStrip12(t *testing.T) {
+	result := StripHTML([]byte("<<"))
 	if string(result) == "" {
 		t.Log("Extended sentence << returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte(">>"))
+}
+func TestStrip13(t *testing.T) {
+	result := StripHTML([]byte(">>"))
 	if string(result) == ">>" {
 		t.Log("Extended sentence returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("<>>"))
+}
+func TestStrip14(t *testing.T) {
+	result := StripHTML([]byte("<>>"))
 	if string(result) == ">" {
 		t.Log("Extended sentence <>> returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("<<>"))
+}
+func TestStrip15(t *testing.T) {
+	result := StripHTML([]byte("<<>"))
 	if string(result) == "" {
 		t.Log("Extended sentence <<> returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
-	result = StripHTML([]byte("&;"))
+}
+func TestStrip16(t *testing.T) {
+	result := StripHTML([]byte("&;"))
 	if string(result) == "" {
 		t.Log("Extended sentence &; returned OK")
 	} else {
-		t.Log("Correct sentence returned NOK "+string(result))
+		t.Log("Correct sentence returned NOK " + string(result))
 		t.Fail()
 	}
-
 }
+
